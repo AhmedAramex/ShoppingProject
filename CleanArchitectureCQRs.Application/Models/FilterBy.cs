1251e0d82c8fc7a5ed9c56e20f0c3c2e89d0ce14
Ahmed Alaa <ahmedalallah17@gmail.com>
2024-12-22 12:31:10 +0200

    filteration first step

---
+++ CleanArchitectureCQRs.Application/Models/FilterBy.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitectureCQRs.Application.Models;
+
+public class FilterBy
+{
+    public string? Name { get; set; }
+    public int? MaxPrice { get; set; }
+    public int? MinPrice { get; set; }
+    public string? Color { get; set; }
+    public string? Category { get; set; }
+}
---
+++ CleanArchitectureCQRs.Infrastructure/Filteration/Filteration.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchitectureCQRs.Application.Models;
+using CleanArchitectureCQRs.Domain.Abstractions;
+using CleanArchitectureCQRs.Domain.Entites;
+using CleanArchitectureCQRs.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CleanArchitectureCQRs.Infrastructure.Filteration;
+
+public class Filteration<T> where T : BaseEntity
+{
+    private readonly ApplicationDbContext _dbcontect;
+
+    public Filteration(ApplicationDbContext dbContext)
+    {
+        _dbcontect = dbContext;
+    }
+    public IQueryable<Product> Filtering(FilterBy filterBy, List<string> SearchCreiteria)
+    {
+        var BaseQuery  = _dbcontect.Products;
+
+        if (!string.IsNullOrEmpty(filterBy.Name))
+            BaseQuery.Where(p => p.Name == filterBy.Name);
+
+        if (filterBy.MaxPrice is not null)
+            BaseQuery.Where(p => p.Price == filterBy.MaxPrice);
+
+        var result  = BaseQuery.ToList();
+
+        
+            var Dic = new Dictionary<string, Func<IQueryable<Product>, List<Product>>>()
+        {
+            { "Name" , Product => Product.Where(p => p.Name == "Name").ToList()}
+
+        };
+
+        return null;
+
+    }
+
+
+    //public Expression<Func<T, bool>> DynamicFilteration()
+    //{
+
+    //}
+
+    //public  IQueryable<T> Filter<T>(this IQueryable<T> query, bool apply, Expression<Func<T, bool>> predicate)
+    //  => apply ? query.Where(predicate) : query;
+}
